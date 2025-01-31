0ee688a97ff7be53267d7e528ce69d043acb752b
Hamster <andrew@nxweb.com>
2025-01-31 09:07:59 -0500

    fix types

---
+++ src/types/globals.d.ts
@@ -0,0 +1,8 @@
+declare module "@mercuryworkshop/epoxy-transport" {
+  export const epoxyPath: string;
+}
+
+declare module "@mercuryworkshop/bare-as-module3" {
+  export const bareModulePath: string;
+}
+
