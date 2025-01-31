8a54b501aab19dc3f8151684c6db92e36cac41d6
alivehamster <andrew@nxweb.com>
2025-01-31 15:59:41 -0500

    update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-import vueTsEslintConfig from "@vue/eslint-config-typescript";
 import pluginAstro from "eslint-plugin-astro";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["src/**/*.{js,ts,vue,astro}"] },
-  { files: ["index.ts", "server/**/*.{js,ts}"] },
-  { ignores: ["**/*", "!src/**", "!index.ts", "!server/**"] },
+  { files: ["index.ts"] },
+  { ignores: ["**/*", "!src/**", "!index.ts"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  ...pluginAstro.configs.recommended,
   {
     rules: {
       "@typescript-eslint/ban-ts-comment": [
@@ -21,9 +24,5 @@ export default [
       ],
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  ...vueTsEslintConfig(),
-  pluginAstro.configs.recommended,
+
 ];
--- src/components/settings/TitleSettings.vue
+++ src/components/settings/TitleSettings.vue
@@ -16,7 +16,7 @@ onMounted(getLocalstorage)
 </script>
 
 <template>
-    <div class="flex flex-col justify-center items-center mb-36">
+  <div class="flex flex-col justify-center items-center mb-36">
     <h1 class="text-white text-5xl font-semibold pt-12">Title Change</h1>
     <input v-model="text" class="w-textw h-16 m-3 font-poppins rounded-3xl text-2xl" type="input" @input="stuff" />
   </div>
