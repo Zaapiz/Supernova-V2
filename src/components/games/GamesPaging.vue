6f68a751f41cf441c3f744e2c72e79d258983528
Hamster <andrew@nxweb.com>
2025-04-10 10:12:45 -0400

    fix button

--- src/components/games/GamesPaging.vue
+++ src/components/games/GamesPaging.vue
@@ -4,34 +4,34 @@ import { page, items, totalPages } from './Games'
 
 <template>
   <div class="flex justify-center items-center text-xl font-bold font-poppins text-white m-3">
-    <button class="m-1" @click="page(1)">&lt;&lt;</button>
+    <button class="m-1 hover:cursor-pointer" @click="page(1)">&lt;&lt;</button>
     <button
       v-if="items.pagenum == totalPages && items.pagenum - 2 > 0"
-      class="m-1 w-8"
+      class="m-1 w-8 hover:cursor-pointer"
       @click="page(items.pagenum - 2)"
     >
       {{ items.pagenum - 2 }}
     </button>
-    <button v-if="items.pagenum - 1 > 0" class="m-0.5 w-8" @click="page(--items.pagenum)">
+    <button v-if="items.pagenum - 1 > 0" class="m-0.5 w-8 hover:cursor-pointer" @click="page(--items.pagenum)">
       {{ items.pagenum - 1 }}
     </button>
-    <button class="m-0.5 p-1 rounded-full bg-title-blue w-10">
+    <button class="m-0.5 p-1 rounded-full bg-title-blue w-10 hover:cursor-pointer">
       {{ items.pagenum }}
     </button>
     <button
       v-if="items.pagenum != totalPages && totalPages != 0"
-      class="m-0.5 w-8"
+      class="m-0.5 w-8 hover:cursor-pointer"
       @click="page(++items.pagenum)"
     >
       {{ items.pagenum + 1 }}
     </button>
     <button
       v-if="items.pagenum - 1 <= 0 && totalPages > 2"
-      class="m-0.5 w-8"
+      class="m-0.5 w-8 hover:cursor-pointer"
       @click="page(items.pagenum + 2)"
     >
       {{ items.pagenum + 2 }}
     </button>
-    <button class="m-1" @click="page(totalPages)">>></button>
+    <button class="m-1 hover:cursor-pointer" @click="page(totalPages)">>></button>
   </div>
 </template>
