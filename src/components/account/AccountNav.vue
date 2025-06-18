0c6bc0ef659ae94a1f1b3ef0b304ca4e87e12930
alivehamster <andrew@nxweb.com>
2025-06-18 16:35:50 -0400

    idk

--- src/components/account/AccountNav.vue
+++ src/components/account/AccountNav.vue
@@ -9,7 +9,7 @@ async function logout() {
     if (!actions?.accountActions) {
       throw new Error('AI actions not available');
     }
-    const status = await actions.accountActions.logout()
+    const status = await actions.accountActions.logout("random data that should not be necessary but idk")
     if (status.data?.status === 'success') {
       window.location.reload()
     }
